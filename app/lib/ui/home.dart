cb4af69c70f4688fdd499a3a37c3acb296e21e20
Han Yin <naco.siren.2012@gmail.com>
2019-04-02 21:59:49 -0700

    Added app bar action items.

--- app/lib/ui/home.dart
+++ app/lib/ui/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:mogicians_manual/ui/tabs.dart';
 import 'package:mogicians_manual/data/models.dart';
@@ -26,8 +29,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.school), // Image.asset('assets/images/ic_guy_fawkes.png'),
+        leading: Icon(MdiIcons.guyFawkesMask),
         title: Text(widget.title),
+        actions: _getAppbarActions(),
       ),
       backgroundColor: Colors.grey.shade200,
       body: Center(
@@ -64,26 +68,95 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Widget _getBottomNav() =>
-    BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(title: Text('【说】'), icon: Icon(Icons.mic)),
-        BottomNavigationBarItem(title: Text('【学】'), icon: Icon(Icons.local_library)),
-        BottomNavigationBarItem(title: Text('【逗】'), icon: Icon(Icons.sentiment_very_satisfied)),
-        BottomNavigationBarItem(title: Text('【唱】'), icon: Icon(Icons.music_note)),
-      ],
-      currentIndex: _selectedIndex,
-      onTap: _selectTabItem,
-    );
+  List<Widget> _getAppbarActions() {
+    const options = <ActionOption>[
+      ActionOption(
+        title: '源码',
+        iconData: MdiIcons.githubCircle,
+        firstUrl: 'https://github.com/naco-siren/mogicians-manual',
+      ),
+      ActionOption(
+        title: '反馈',
+        iconData: Icons.bug_report,
+        firstUrl: 'https://github.com/naco-siren/mogicians-manual/issues',
+      ),
+      ActionOption(
+        title: '开发者',
+        iconData: Icons.person,
+        firstUrl: 'zhihu://people/naco_siren',
+        secondUrl: 'https://www.zhihu.com/people/naco_siren',
+      ),
+    ];
 
-  void _selectTabItem(int index) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(options[0].iconData),
+        onPressed: () => _launchUrl(options[0]),
+      ),
+      IconButton(
+        icon: Icon(options[1].iconData),
+        onPressed: () => _launchUrl(options[1]),
+      ),
+      PopupMenuButton<ActionOption>(
+        onSelected: (option) => _launchUrl(option),
+        itemBuilder: (BuildContext context) {
+          return options.skip(2).map((ActionOption option) {
+            return PopupMenuItem<ActionOption>(
+              value: option,
+              child: Text(option.title),
+            );
+          }).toList();
+        },
+      ),
+    ];
+  }
+
+  _launchUrl(ActionOption option) async {
+    if (await canLaunch(option.firstUrl)) {
+      await launch(option.firstUrl);
+    } else if (option.secondUrl != null && await canLaunch(option.secondUrl)) {
+      await launch(option.secondUrl);
+    } else {
+      Fluttertoast.showToast(
+          msg: 'Deep ♂ Dark ♂ Fantasy',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 1,
+          backgroundColor: Colors.grey.shade700.withOpacity(0.9),
+          textColor: Colors.white,
+          fontSize: 14.0);
+    }
+  }
+
+  Widget _getBottomNav() => BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(title: Text('【说】'), icon: Icon(Icons.mic)),
+          BottomNavigationBarItem(title: Text('【学】'), icon: Icon(Icons.local_library)),
+          BottomNavigationBarItem(title: Text('【逗】'), icon: Icon(Icons.sentiment_very_satisfied)),
+          BottomNavigationBarItem(title: Text('【唱】'), icon: Icon(Icons.music_note)),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _selectTabItem,
+      );
+
+  _selectTabItem(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  void _selectMusicItem(int index) {
+  _selectMusicItem(int index) {
     _changModel.curIdx = index;
   }
+}
+
+class ActionOption {
+  const ActionOption(
+      {this.title, this.iconData, this.firstUrl, this.secondUrl});
+
+  final String title;
+  final IconData iconData;
+  final String firstUrl;
+  final String secondUrl;
 }
