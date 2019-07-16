ac90b53d77fc493fd348c63f9c24f38cbff8ef03
Han Yin <naco.siren.2012@gmail.com>
2019-07-15 20:19:07 -0700

    Upgraded packages.

--- app/lib/utils/share_helper.dart
+++ app/lib/utils/share_helper.dart
@@ -4,5 +4,5 @@ import 'package:mogicians_manual/data/list_items.dart';
 
 void shareImage(ImageItem item) async {
   final ByteData bytes = await rootBundle.load(item.path);
-  await EsysFlutterShare.shareImage(item.src, bytes, '发送【${item.title}】');
+  await Share.file('发送【${item.title}】', item.src, bytes.buffer.asUint8List(), 'image/png');
 }
