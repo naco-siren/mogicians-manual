4923345e718b00783ac288ab8ab98bcd675b278e
Han Yin <naco.siren.2012@gmail.com>
2020-08-08 21:55:51 -0700

    Upgrade packages. And remove some images due to Google's content policy.

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/main.dart';
+import 'package:mogicians_manual/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
