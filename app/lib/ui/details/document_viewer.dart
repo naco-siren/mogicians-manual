c2ec97e289b0952d12aa8fa8bc7fd30fe50415be
Han Yin <naco.siren.2012@gmail.com>
2023-01-01 16:59:16 -0800

    Implement document sharing; UI polish

--- app/lib/ui/details/document_viewer.dart
+++ app/lib/ui/details/document_viewer.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
 import 'package:mogicians_manual/data/list_items.dart';
@@ -10,39 +11,47 @@ class DocumentViewer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      FutureBuilder(
-          future: loadDocument(item),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Scaffold(
-                  appBar: AppBar(
-                    leading: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                    actions: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.share),
-                        onPressed: () {
-                          // shareImage(item);
-                          // _toastSharingInfo();
-                        },
-                      ),
-                    ],
-                    title: Text(item.title),
-                  ),
-                  body: Container(
-                      child: PdfView(key: Key(item.title), path: snapshot.data)
-                  ));
-            } else {
-              return const SizedBox(
-                child: CircularProgressIndicator(),
-                height: 40.0,
-                width: 40.0,
-              );
-            }
-          }
-      );
+    return FutureBuilder(
+        future: loadDocument(item),
+        builder: (context, snapshot) {
+          final loaded = snapshot.connectionState == ConnectionState.done;
+          final File file = snapshot.data;
+
+          return Scaffold(
+              appBar: AppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                actions: _appBarActions(loaded, item.title, file),
+                title: Text(item.title),
+              ),
+              body: _content(loaded, file?.path));
+        });
+  }
+
+  List<Widget> _appBarActions(bool loaded, String title, File file) {
+    final List<Widget> actions = [];
+    if (loaded) {
+      actions.add(IconButton(
+        icon: Icon(Icons.share),
+        onPressed: () {
+          shareDocument(title, file);
+        },
+      ));
+    }
+    return actions;
+  }
+
+  Widget _content(bool loaded, String path) {
+    final child = loaded
+        ? PdfView(key: Key(item.title), path: path)
+        : SizedBox(
+            child: CircularProgressIndicator(),
+            height: 80.0,
+            width: 80.0,
+          );
+
+    return Container(child: child, alignment: Alignment.center);
   }
 }
--- app/lib/utils/share_helper.dart
+++ app/lib/utils/share_helper.dart
@@ -11,14 +11,20 @@ void shareImage(ImageItem item) async {
   await Share.file('发送【${item.title}】', item.src, bytes.buffer.asUint8List(), 'image/png');
 }
 
-Future<String> loadDocument(DocumentItem item) async {
+Future<File> loadDocument(DocumentItem item) async {
+  // Double check if the file has already been pulled into internal storage
   final directory = await getApplicationDocumentsDirectory();
   final file = File('${directory.path}/${item.title}.pdf');
   if (await file.exists()) {
-    return file.path;
+    return file;
   }
 
+  // Otherwise, export it from the root asset bundle
   final ByteData bytes = await rootBundle.load(item.path);
   await file.writeAsBytes(bytes.buffer.asUint8List());
-  return file.path;
+  return file;
+}
+
+void shareDocument(String title, File file) async {
+  await Share.file('发送【$title】', title + ".pdf", file.readAsBytesSync(), 'application/pdf');
 }
